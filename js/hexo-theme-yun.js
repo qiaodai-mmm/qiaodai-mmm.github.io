1d36faaeb3ad1d44fbe036cab2e8d24331ea6579
qiaodai-mmm <3021144916@qq.com>
2020-08-28 17:40:00 +0800

    Site updated: 2020-08-28 17:40:00

--- js/hexo-theme-yun.js
+++ js/hexo-theme-yun.js
@@ -1,5 +1,5 @@
 console.log(
-  "%c ☁️ hexo-theme-yun %c https://github.com/YunYouJun/hexo-theme-yun",
+  `%c ☁️ hexo-theme-yun ${CONFIG.version} %c https://github.com/YunYouJun/hexo-theme-yun`,
   "color: white; background: #0078E7; padding:5px 0;",
   "padding:4px;border:1px solid #0078E7;"
 );
@@ -19,20 +19,22 @@ function scrollPercent(curTop) {
 }
 
 function initPage() {
-  if (typeof ScrollReveal !== "undefined") {
-    ScrollReveal().reveal(".post-card");
-  }
-
   // open sidebar
-  document.querySelector(".sidebar-toggle").onclick = function() {
-    this.querySelector(".hamburger").classList.toggle("is-active");
-    document.querySelector(".container").classList.toggle("sidebar-open");
-  };
+  const toggleBtns = document.querySelectorAll(".sidebar-toggle");
+  toggleBtns.forEach((el) => {
+    el.addEventListener("click", () => {
+      document.querySelector(".hamburger").classList.toggle("is-active");
+      document.querySelector(".container").classList.toggle("sidebar-open");
+    });
+  });
 
   window.addEventListener("scroll", function() {
     goUp.classList.toggle("show", window.scrollY > 64);
     scrollPercent(window.scrollY);
   });
+
+  // wrap
+  Yun.utils.wrapTable();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
